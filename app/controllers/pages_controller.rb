0166e2fd0b6fc792ed3164e27057616720a63979
Anik Rigaud <s@tanas.net>
2016-08-24 15:25:27 +0200

    intégration maps

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,11 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @boxes = Box.openables
+    @hash = Gmaps4rails.build_markers(@boxes) do |box, marker|
+      marker.lat box.latitude
+      marker.lng box.longitude
+      # marker.infowindow render_to_string(partial: "/box/map_box", locals: { box: box })
+    end
   end
 end
