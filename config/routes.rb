a43da3e70341c08b676b26775d7ca8da7bc0878f
Yannick Dulous <yannick@fantomass.com>
2016-08-25 14:31:21 +0200

    modification sur les routes des openings

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,4 @@ Rails.application.routes.draw do
   resources :boxes, only: [:new, :create, :index, :show] do
     resources :openings, only: [:create]
   end
-  resources :openings, only: [:edit, :update]
-
 end
