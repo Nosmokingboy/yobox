138e8c06b20daa6d73a755f5b9adcd02be0b1ce3
Yannick Dulous <yannick@fantomass.com>
2016-08-25 10:32:11 +0200

    Changement du home en index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'pages#index'
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
