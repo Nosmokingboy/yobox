2e3d11e40f7744149dd10be7b41bf01366a0cf9b
Anik Rigaud <s@tanas.net>
2016-08-25 13:44:43 +0200

    seed optimisé + ajout URL dans content + erreur 'emoji' fixée

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,116 +1,66 @@
 require 'faker'
 
-# ménage
-Opening.destroy_all
-Box.destroy_all
-User.destroy_all
-puts "Ménage effectué !"
+  # ménage
+  Opening.destroy_all
+  Box.destroy_all
+  User.destroy_all
+  puts "Ménage effectué !"
 
-users    = []
-boxes    = []
-openings = []
+  users    = []
+  b1, b2, b3, b4 = [], [], [], []
+  openings = []
 
-100.times do
-  u = User.new
-  u.last_name  = Faker::Name.last_name.upcase
-  u.first_name = Faker::Name.first_name.capitalize
-  u.email = Faker::Internet.email
-  u.password = 'totototo'
-  u.save
-  users << u
-end
-puts "Users générés !"
+  100.times do
+    u = User.new
+    u.last_name  = Faker::Name.last_name.upcase
+    u.first_name = Faker::Name.first_name.capitalize
+    u.email = Faker::Internet.email
+    u.password = 'totototo'
+    u.save
+    users << u
+  end
+  puts "#{users.count} users générés !"
 
-max_lat = 50.00000
-min_lat = -35.00000
-max_lng = 145.00000
-min_lng = -120.00000
-#-----------#
-100.times do
-  b = Box.new
-  b.title = Faker::Book.title
-  b.content = Faker::Lorem.paragraph(2)
-  b.latitude = rand(min_lat..max_lat)
-  b.longitude = rand(min_lng..max_lng)
-  b.expiration_date_time = Faker::Time.between(DateTime.now - 7, DateTime.now + 7)
-  b.icon = Faker::SlackEmoji
-  b.openings_max = rand(1..2) == 1 ? nil : rand(1..7) # 1 fois sur 2, pas de nb limite
-  b.user = users.sample
-  b.save
-  boxes << b
-end
-puts "Boxs mondiales générées !"
+  def create_boxes(min_lat, max_lat, min_lng, max_lng, number, reach, users)
+    res = Array.new
+    number.times do
+      b = Box.new
+      b.title = Faker::Book.title
+      b.content = "#{Faker::Lorem.paragraph(2)}\n#{Faker::Internet.url}"
+      b.latitude = rand(min_lat..max_lat)
+      b.longitude = rand(min_lng..max_lng)
+      b.expiration_date_time = Faker::Time.between(DateTime.now - 7, DateTime.now + 7)
+      b.icon = Faker::SlackEmoji.emoji
+      b.openings_max = rand(1..2) == 1 ? nil : rand(1..7) # 1 fois sur 2, pas de nb limite
+      b.user = users.sample
+      b.save
+      res << b
+    end
+    puts "#{res.length} boxes générées (#{reach})"
+    res
+  end
 
-max_lat = 50.00000
-min_lat = 44.00000
-max_lng = 9.00000
-min_lng = -5.00000
-#-----------#
-200.times do
-  b = Box.new
-  b.title = Faker::Book.title
-  b.content = Faker::Lorem.paragraph(2)
-  b.latitude = rand(min_lat..max_lat)
-  b.longitude = rand(min_lng..max_lng)
-  b.expiration_date_time = Faker::Time.between(DateTime.now - 7, DateTime.now + 7)
-  b.icon = Faker::SlackEmoji
-  b.openings_max = rand(1..2) == 1 ? nil : rand(1..7) # 1 fois sur 2, pas de nb limite
-  b.user = users.sample
-  b.save
-  boxes << b
-end
-puts "Boxs françaises générées !"
+  b1 = create_boxes(-35.00000, 50.00000, -120.000, 145.000,  100, "world",     users)
+  b2 = create_boxes(44.00000,  50.00000, -5.00000, 9.00000,  200, "france",    users)
+  b3 = create_boxes(44.00000,  45.50000, -1.15000, 1.02000,  200, "aquitaine", users)
+  b4 = create_boxes(44.81000,  44.86000, -0.60320, -0.52450, 300, "bordeaux",  users)
 
-max_lat = 45.50000
-min_lat = 44.00000
-max_lng = 1.02000
-min_lng = -1.15000
-#-----------#
-200.times do
-  b = Box.new
-  b.title = Faker::Book.title
-  b.content = Faker::Lorem.paragraph(2)
-  b.latitude = rand(min_lat..max_lat)
-  b.longitude = rand(min_lng..max_lng)
-  b.expiration_date_time = Faker::Time.between(DateTime.now - 7, DateTime.now + 7)
-  b.icon = Faker::SlackEmoji
-  b.openings_max = rand(1..2) == 1 ? nil : rand(1..7) # 1 fois sur 2, pas de nb limite
-  b.user = users.sample
-  b.save
-  boxes << b
-end
-puts "Boxs aquitaines générées !"
+  (b1 << b2 << b3 << b4).flatten!
+  puts "(#{b1.count} boxs au total)"
 
-max_lat = 44.86000
-min_lat = 44.81000
-max_lng = -0.52450
-min_lng = -0.60320
-#-----------#
-200.times do
-  b = Box.new
-  b.title = Faker::Book.title
-  b.content = Faker::Lorem.paragraph(2)
-  b.latitude = rand(min_lat..max_lat)
-  b.longitude = rand(min_lng..max_lng)
-  b.expiration_date_time = Faker::Time.between(DateTime.now - 7, DateTime.now + 7)
-  b.icon = Faker::SlackEmoji
-  b.openings_max = rand(1..2) == 1 ? nil : rand(1..7) # 1 fois sur 2, pas de nb limite
-  b.user = users.sample
-  b.save
-  boxes << b
-end
-puts "Boxs bordelaises générées !"
-
-1000.times do
-  o = Opening.new
-  o.box = boxes.sample
-  o.user = users.sample
-  o.rating = rand(1..4) == 1 ? nil : rand(0..5) # 1 fois sur 4, pas de rating laissé
-  o.save
-  openings << o
-end
-puts "Openings générées !"
+  800.times do
+    o = Opening.new
+    o.box = b1.sample
+    o.user = users.sample
+    o.rating = rand(1..4) == 1 ? nil : rand(0..5) # 1 fois sur 4, pas de rating laissé
+    o.save
+    openings << o
+  end
+  puts "#{Opening.all.count} openings générées !"
 
 # p users
-# p boxes
+# p b1
+# p b2
+# p b3
+# p b4
 # p openings
