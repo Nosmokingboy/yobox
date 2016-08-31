284ce4cbcafaab19751589d5e4d2eb20ce202409
Anik Rigaud <s@tanas.net>
2016-08-31 17:37:19 +0200

    cercle et carte

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -2,14 +2,6 @@ class BoxesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :preview ]
 
   def index
-    # TODO: fix map build in index = DONE :-)
-    # @boxes = Box.all.near("38 place de la Bourse 33000", 2).openables
-    # @hash = Gmaps4rails.build_markers(@boxes) do |box, marker|
-    #   marker.lat box.latitude
-    #   marker.lng box.longitude
-    #   marker.title box.id.to_s
-    #   marker.infowindow render_to_string(partial: "/boxes/infowindow", locals: { box: box })
-    # end
   end
 
   def create
@@ -43,7 +35,7 @@ class BoxesController < ApplicationController
     latitude  = params[:lat]
     longitude = params[:lng]
     @box = Box.find(params[:id])
-    @open = @box.is_unlockable?(latitude, longitude, 1) # 1: distance in km
+    @open = @box.is_unlockable?(latitude, longitude, 0.3) # radius in km
     @distance = @box.box_distance(latitude, longitude)
     render layout: false
   end
