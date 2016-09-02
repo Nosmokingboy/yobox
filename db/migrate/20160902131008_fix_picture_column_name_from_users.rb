322a33727cd262920980c1bc0d955133ab5d9744
Yannick Dulous <yannick@fantomass.com>
2016-09-02 17:06:39 +0200

    fix

--- db/migrate/20160902131008_fix_picture_column_name_from_users.rb
+++
@@ -1,5 +0,0 @@
-class FixPictureColumnNameFromUsers < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :users, :picture_url, :picture
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160902131008) do
+ActiveRecord::Schema.define(version: 20160902103012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
